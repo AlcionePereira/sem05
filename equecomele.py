91f09649e6499687baa423d31773b647a8e3b194
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-30 15:59:58 -0300

    Add files via upload

---
+++ equecomele.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
+m = (num1+num2+num3+num4+num5) / 5
+
+print(max(num1, num2 ,num3 ,num4, num5))
+print(min(num1, num2, num3, num4, num5))
+print(m)
+          
+          
+
+
+      
+
+
+
+      
+
+
+
+
+
+
+
+             
+             
+      
+
+
+
+
+
+
+
+
+
