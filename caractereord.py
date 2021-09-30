83c233e044d4ec778575a4f917c7d5ca451287a1
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-30 00:07:30 -0300

    Add files via upload

---
+++ caractereord.py
@@ -0,0 +1,20 @@
+#02. Escreva um programa que leia um único caractere pelo teclado e informe o código numérico correspondente ao caractere lido.
+
+
+
+
+
+
+codigo = int()
+codigo= input()
+print(ord(codigo))
+
+
+
+
+      
+
+
+
+#gostei dessas funçoes embutidas, facilitam muito.
+      
